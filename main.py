99fadfee3cbaa894b7a61775a26c62deefd5e32f
Ashok <nashok@cisco.com>
2020-08-30 18:04:51 +0530

    flake8

--- main.py
+++ main.py
@@ -5,23 +5,22 @@ import argparse
 import op
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-op', '--op', required=True, choices=['add', 'sub', 'mul', 'div'])
-	parser.add_argument('-a', type=int, required=True)
-	parser.add_argument('-b', type=int, required=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-op', '--op', required=True,
+                        choices=['add', 'sub', 'mul', 'div'])
+    parser.add_argument('-a', type=int, required=True)
+    parser.add_argument('-b', type=int, required=True)
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
-	func(a, b)
-	result = None
-	if op == 'add':
-		result = op.add(a, b)
-	elif op == 'sub':
-		result = op.sub(a, b)
-	elif op == 'mul':
-		result = op.mul(a, b)
-	else:
-		result = op.div(a, b)
+    result = None
+    if args.op == 'add':
+        result = op.add(args.a, args.b)
+    elif args.op == 'sub':
+        result = op.sub(args.a, args.b)
+    elif args.op == 'mul':
+        result = op.mul(args.a, args.b)
+    else:
+        result = op.div(args.a, args.b)
 
-	print(f'do({args.a} {args.op} {args.b}) = {result}')
-
+    print(f'do({args.a} {args.op} {args.b}) = {result}')
